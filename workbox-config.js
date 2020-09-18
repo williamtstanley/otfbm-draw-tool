4ff19397a327eb53d45b76c60bad77f7dbc7e795
Travis Stanley <travis@livingskytech.com>
2020-09-18 15:23:19 -0700

    add png to the workbox glob pattern

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "globDirectory": "build/",
   "globPatterns": [
-    "**/*.{js,css,ico,html,txt,json}"
+    "**/*.{js,css,png,ico,html,txt,json}"
   ],
   "swDest": "build/sw.js"
 };
